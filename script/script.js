a60d4cdb2cdaf9942aae039d1b66a4a8f6f6c394
Mohammad Kassem <mohammaddkassemm@gmail.com>
2022-09-16 16:36:32 +0300

    switching cards to its back

--- script/script.js
+++ script/script.js
@@ -1,1 +1,16 @@
+const cards = document.getElementsByClassName("cards")
+const start = document.getElementById("start-btn")
+
+start.addEventListener("click",() => {
+    for(let i=0;i < cards.length ; i++){
+        cards[i].src= "./assets/card back.png"
+    }
+})
+
+// for(let i=0;i < cards.length ; i++){
+//     cards[i].addEventListener("toggle",()=>{
+//         cards[i].src =  
+//     }
+//     )
+// }
 
