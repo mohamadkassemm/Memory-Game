b7a0af73e61d89a43dfcf0f7bd47b7f3937cb868
Mohammad Kassem <mohammaddkassemm@gmail.com>
2022-09-16 17:10:35 +0300

    returning image to its default on click

--- script/script.js
+++ script/script.js
@@ -1,16 +1,20 @@
 const cards = document.getElementsByClassName("cards")
 const start = document.getElementById("start-btn")
+const array=[]
 
+for(let i=0;i < cards.length ; i++){
+    array.push(cards[i].src)
+}
+console.log(array)
 start.addEventListener("click",() => {
     for(let i=0;i < cards.length ; i++){
         cards[i].src= "./assets/card back.png"
     }
 })
 
-// for(let i=0;i < cards.length ; i++){
-//     cards[i].addEventListener("toggle",()=>{
-//         cards[i].src =  
-//     }
-//     )
-// }
+for(let i=0;i < cards.length ; i++){
+    cards[i].addEventListener("click",()=>{
+        cards[i].src = array[i]
+    })
+}
 
