e5a60bbad261f556fc472cd20fcd31d827574aaf
Mohammad Kassem <mohammaddkassemm@gmail.com>
2022-09-16 20:21:52 +0300

    bugged game but no time, sorry

--- script/script.js
+++ script/script.js
@@ -2,19 +2,35 @@ const cards = document.getElementsByClassName("cards")
 const start = document.getElementById("start-btn")
 const array=[]
 
+// saving the default sources
 for(let i=0;i < cards.length ; i++){
     array.push(cards[i].src)
 }
-console.log(array)
+// after clicking start the back image will appear
 start.addEventListener("click",() => {
     for(let i=0;i < cards.length ; i++){
         cards[i].src= "./assets/card back.png"
     }
+    start.innerText="Restart Game"
 })
-
+// when clicking on the image the default image appears 
+let counter = 0
 for(let i=0;i < cards.length ; i++){
     cards[i].addEventListener("click",()=>{
         cards[i].src = array[i]
-    })
+        if(counter==0){
+            A= cards[i].src
+        }else if(counter ==1){
+            B= cards[i].src
+        }
+            counter +=1
+            if(counter==2){
+                counter=0
+                if(A==B){
+                    cards[i].src= "./assets/faded.png"
+                }else{
+                    cards[i].src="./assets/card back.png"
+                }       
+        }
+})
 }
-
